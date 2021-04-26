5d60f704f309df46b803d41c68cfee38091c11a7
TrongHoa <34540606+Nagaruu@users.noreply.github.com>
2021-04-26 13:42:59 +0700

    done

---
+++ web/js/view/message.js
@@ -0,0 +1,42 @@
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+ define([
+    'uiComponent',
+    'Magento_Customer/js/customer-data',
+    'jquery',
+    'ko',
+    'underscore',
+    'sidebar',
+    'mage/translate',
+    'mage/dropdown'
+], function (Component, customerData, $, ko, _) {
+    'use strict';
+
+    return Component.extend({
+        defaults: {
+            template: 'AHT_Catwalk/minicart/content_message',
+        },
+        freeShippingSub: window.checkout.freeShippingSubtotal,
+        sym_bol: window.checkout.symbol,
+        messs: window.checkout.mess,
+        mess_freee: window.checkout.mess_free,
+        messfree: function () {
+            var subtotalAmount= customerData.get('cart')().subtotalAmount;
+            var freeShippingSub =this.freeShippingSub;
+            var sym_bol =this.sym_bol;
+            var mess_freee =this.mess_freee;
+            var message = "You are "+ sym_bol + (freeShippingSub-subtotalAmount) + " away from free shipping";
+
+            if(Number(subtotalAmount) < Number(freeShippingSub) && subtotalAmount!=0 ){
+                return message;
+            }else if(Number(subtotalAmount) >= Number(freeShippingSub)){
+                return mess_freee;
+            }else {
+                return null;
+            }
+        }
+    });
+});
